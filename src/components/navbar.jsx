2f7451836c7e10bb75f49f53f11425addb9b5d3d
zeeforum <zartashzulfiqar@gmail.com>
2020-10-12 18:25:30 +0500

    Change NavBar Title

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 
 class NavBar extends Component {
-  state = {};
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a href="/home" className="navbar-brand">
-          Navbar&nbsp;
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounter}
-          </span>
-        </a>
-      </nav>
-    );
-  }
+	state = {};
+	render() {
+		return (
+			<nav className="navbar navbar-light bg-light">
+				<a href="/home" className="navbar-brand">
+					Zartash Zulfiqar&nbsp;
+					<span className="badge badge-pill badge-secondary">
+						{this.props.totalCounter}
+					</span>
+				</a>
+			</nav>
+		);
+	}
 }
 
 export default NavBar;
